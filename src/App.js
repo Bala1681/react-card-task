b0e0f042ab80662607ea63f1c804809fcc827177
Balavignesh Sarveshwaran <balavigneshsarveshwaran@AGLS-MLT-469.local>
2022-03-02 09:53:30 +0530

    price app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Card from "./Components/card";
+import React from "react";
+const bala=()=>{
+ return (
+   <div>
+     <Card></Card>
+    
+     
+   </div>
+  
+ )
+ 
+}
+
+
+
+export default bala;
